e93f80985a6bdd0e6e64040e32db393f8c8a502c
Vinícius dos Santos Oliveira <vinicius@blinktrade.com>
2019-12-17 00:25:31 -0300

    Remove unused barrier tests helper

--- test/sync.cpp
+++ test/sync.cpp
@@ -13,7 +13,6 @@
 #include <boost/asio/steady_timer.hpp>
 
 #include <trial/iofiber/mutex.hpp>
-#include "barrier.hpp"
 
 namespace asio = boost::asio;
 using namespace trial;
@@ -93,76 +92,4 @@ BOOST_AUTO_TEST_CASE(mutex) {
     // to rely on FIFO semantics.
     std::vector<int> expected{-1, -2, 101, 102, 301, 302, 201, 202, 311, -3};
     BOOST_REQUIRE(output == expected);
-}
-
-BOOST_AUTO_TEST_CASE(barrier) {
-    boost::asio::io_context ios;
-
-    std::vector<bool> values(3, false);
-    iofiber::fiber_barrier barrier(ios, 3);
-
-    auto w1 = [&](iofiber::fiber::this_fiber this_fiber) {
-        boost::asio::steady_timer timer{ios};
-        timer.expires_after(std::chrono::seconds(1));
-        timer.async_wait(this_fiber);
-
-        BOOST_REQUIRE(values[0] == false);
-        BOOST_REQUIRE(values[1] == false);
-        BOOST_REQUIRE(values[2] == false);
-
-        values[0] = true;
-        BOOST_REQUIRE(barrier.wait(this_fiber) == false);
-
-        BOOST_REQUIRE(values[0] == true);
-        BOOST_REQUIRE(values[1] == true);
-        BOOST_REQUIRE(values[2] == true);
-    };
-
-    auto w2 = [&](iofiber::fiber::this_fiber this_fiber) {
-        boost::asio::steady_timer timer{ios};
-        timer.expires_after(std::chrono::seconds(2));
-        timer.async_wait(this_fiber);
-
-        BOOST_REQUIRE(values[0] == true);
-        BOOST_REQUIRE(values[1] == false);
-        BOOST_REQUIRE(values[2] == false);
-
-        values[1] = true;
-        BOOST_REQUIRE(barrier.wait(this_fiber) == false);
-
-        BOOST_REQUIRE(values[0] == true);
-        BOOST_REQUIRE(values[1] == true);
-        BOOST_REQUIRE(values[2] == true);
-    };
-
-    auto w3 = [&](iofiber::fiber::this_fiber this_fiber) {
-        boost::asio::steady_timer timer{ios};
-        timer.expires_after(std::chrono::seconds(3));
-        timer.async_wait(this_fiber);
-
-        BOOST_REQUIRE(values[0] == true);
-        BOOST_REQUIRE(values[1] == true);
-        BOOST_REQUIRE(values[2] == false);
-
-        values[2] = true;
-        BOOST_REQUIRE(barrier.wait(this_fiber) == true);
-
-        BOOST_REQUIRE(values[0] == true);
-        BOOST_REQUIRE(values[1] == true);
-        BOOST_REQUIRE(values[2] == true);
-    };
-
-    iofiber::spawn(ios, w1).detach();
-    iofiber::spawn(ios, w2).detach();
-    iofiber::spawn(ios, w3).detach();
-
-    BOOST_REQUIRE(values[0] == false);
-    BOOST_REQUIRE(values[1] == false);
-    BOOST_REQUIRE(values[2] == false);
-
-    ios.run();
-
-    BOOST_REQUIRE(values[0] == true);
-    BOOST_REQUIRE(values[1] == true);
-    BOOST_REQUIRE(values[2] == true);
 }
